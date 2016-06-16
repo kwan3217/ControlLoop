9a739346b769e3ea0ca216f1bef93ec4387d9b7f
Chris Jeppesen <chrisj@mio.kwansystems.org>
2016-06-16 17:18:58 -0600

    Added closed loop navigation and OneWaypoint guidance. It doesn't know when to stop yet.

---
+++ ClosedLoopRobotController.py
@@ -0,0 +1,19 @@
+from Robot import RobotController
+
+class ClosedLoopRobotController(RobotController):
+    def __init__(self,interface):
+        RobotController.__init__(self, interface)
+        #Set up the part of the state vector estimate that the controller cares about
+        self.heading=0.0;
+        #Set up the guidance vector
+        self.cmdHeading=0.0;
+        #Set up the steering control constants
+        self.P=-20;
+    def control(self):
+        stCmd=self.P*(self.heading-self.cmdHeading)
+        if stCmd>1:
+            stCmd=1
+        elif stCmd<-1:
+            stCmd=-1
+        self.interface.steer(stCmd)
+                

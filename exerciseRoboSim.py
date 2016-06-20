c3da49e1c1eefa8604c7279fdd412f8af485399b
kwan3217 <kwan3217@gmail.com>
2016-06-20 15:33:23 -0600

    Fix issue #6

--- exerciseRoboSim.py
+++ exerciseRoboSim.py
@@ -4,7 +4,11 @@ import numpy
 
 #Instantiate the robot simulator                
 robosim=RoboSim('OneWaypoint.csv',WaypointRobotController.stateheading)
-robocontrol=WaypointRobotController(robosim,[numpy.array([4000.0,4000.0]),numpy.array([0.0,0.0])])
+robocontrol=WaypointRobotController(robosim,[
+    numpy.array([500.0,3000.0]),
+    numpy.array([4000.0,2900.0]),
+    numpy.array([3500.0,-500.0]),
+    numpy.array([0.0,0.0])])
 robosim.throttle.write(1)
 
 #Main loop - let the robot NGC run, then step the simulation
